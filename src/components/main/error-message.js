b5a8aca3eef722e9186ef1c11dc92fc37aa07895
Dmytro Kovalenko <kovalenko.d.y@gmail.com>
2017-08-06 13:51:24 +0200

    Fixed bug with css conflict

--- src/components/main/error-message.js
+++ src/components/main/error-message.js
@@ -10,30 +10,32 @@ export default class ErrorMessage extends Component {
   render() {
     if (this.props.showError) {
       return (
-        <div className='error-background'>
-          <div className='error-window'>
-            <i
-              className="fa fa-times close"
-              aria-hidden="true"
-              onClick={this.closeErrorMessage.bind(this)}></i>
-            <h1>Oops...</h1>
+        <div className='error-message'>
+          <div className='error-background'>
+            <div className='error-window'>
+              <i
+                className="fa fa-times close"
+                aria-hidden="true"
+                onClick={this.closeErrorMessage.bind(this)}></i>
+              <h1>Oops...</h1>
 
-            <p>We got an error, trying to load a question from the server.
-              Looks like your browser is blocking mixed https/http requests.
-              To avoid this problem, try to load this page over http.
-            </p>
+              <p>We got an error, trying to load a question from the server.
+                Looks like your browser is blocking mixed https/http requests.
+                To avoid this problem, try to load this page over http.
+              </p>
 
-            <div>
-              <h4>
-                To load this page over HTTP click the picture below:
-              </h4>
+              <div>
+                <h4>
+                  To load this page over HTTP click the picture below:
+                </h4>
 
-              <a
-                className='tip-image-chrome picture'
-                href='http://vast-thicket-75327.herokuapp.com/'>
-              </a>
+                <a
+                  className='tip-image-chrome picture'
+                  href='http://vast-thicket-75327.herokuapp.com/'>
+                </a>
+              </div>
+
             </div>
-
           </div>
         </div>
       )
