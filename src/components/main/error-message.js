cc617c2793bba9bd8868f84ac87a590a1c7c3498
Dmytro Kovalenko <kovalenko.d.y@gmail.com>
2017-08-06 13:17:29 +0200

    Finished murk-up part of the error page

--- src/components/main/error-message.js
+++ src/components/main/error-message.js
@@ -7,13 +7,25 @@ export default class ErrorMessage extends Component {
     return (
       <div className='error-background'>
         <div className='error-window'>
+          <i className="fa fa-times close" aria-hidden="true"></i>
           <h1>Oops...</h1>
 
           <p>We got an error, trying to load a question from the server.
-            Looks like your browser is blocking http request.
-            To avoid this problem, you should disable default
-            protection of your browser:
+            Looks like your browser is blocking mixed https/http requests.
+            To avoid this problem, try to load this page over http.
           </p>
+
+          <div>
+            <h4>
+              To load this page over HTTP click the picture below:
+            </h4>
+
+            <a
+              className='tip-image-chrome picture'
+              href='http://vast-thicket-75327.herokuapp.com/'>
+            </a>
+          </div>
+
         </div>
       </div>
     )
