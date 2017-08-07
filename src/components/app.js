23c632a5f8d043d8e44d9cf9244779b7b047efda
Dmytro Kovalenko <kovalenko.d.y@gmail.com>
2017-08-07 21:15:20 +0200

    Improved navbar - now active button dinamicaly changes

--- src/components/app.js
+++ src/components/app.js
@@ -10,7 +10,7 @@ export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <Header />
+        <Header location={this.props.location.pathname}/>
         <div className='container'>
           {this.props.children}
         </div>
--- src/components/header.js
+++ src/components/header.js
@@ -3,15 +3,37 @@ import React, { Component } from 'react';
 
 // This component is a simple header for the page
 export default class Header extends Component {
+
+  makeActiveBasedOnPath(path) {
+    if (this.props.location === path) {
+      return "active";
+    }
+    return "";
+  }
+
   render() {
+    const path = {
+      home: "/",
+      about: "/about",
+      contact: "/contact"
+    }
     return (
       <div className='header'>
         <div className="clearfix header-info">
           <nav className="header-buttons">
             <ul className="nav nav-pills pull-right">
-              <li role="presentation" className="active"><a href="/">Home</a></li>
-              <li role="presentation"><a href="/about">About</a></li>
-              <li role="presentation"><a href="/contact">Contact</a></li>
+              <li role="presentation"
+                  className={this.makeActiveBasedOnPath(path.home)}>
+                  <a href={path.home}>Home</a>
+              </li>
+              <li role="presentation"
+                  className={this.makeActiveBasedOnPath(path.about)}>
+                  <a href={path.about}>About</a>
+              </li>
+              <li role="presentation"
+                  className={this.makeActiveBasedOnPath(path.contact)}>
+                  <a href={path.contact}>Contact</a>
+              </li>
             </ul>
           </nav>
           <h3 className="text-muted project-name">SUPER QUIZ</h3>
